e52bbccd05beadb4e6e8260b35d5e8533e65dbc4
Daniel Kumazawa Morais <126463272+bioinfo-arctic@users.noreply.github.com>
2026-05-02 08:22:01 +0200

    Fix window scroll when pressing arrows

--- input.js
+++ input.js
@@ -1,10 +1,12 @@
 const keys = {};
 
 window.addEventListener('keydown', (e) => {
+  if (['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'].includes(e.key)) {
+    e.preventDefault(); // <-- this is the missing piece
+  }
   keys[e.key] = true;
 });
 
 window.addEventListener('keyup', (e) => {
   keys[e.key] = false;
 });
-
